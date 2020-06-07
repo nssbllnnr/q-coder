c77c75daf3bd7f10f51c26dd5af4aa6019bd1cd6
AiselUSMANOVA <160107054@stu.sdu.edu.kz>
2020-06-07 20:45:15 +0600

    add quizzes

--- qmain/urls.py
+++ qmain/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, re_path, include
-from .views import main, courses, assignments
+from .views import main, courses, assignments, quizzes
+
 
 urlpatterns = [
     path('', main.MainView.as_view(), name='main'),
@@ -11,4 +12,8 @@ urlpatterns = [
     path('course/<int:id>/tasks', assignments.course, name='course'),
     path('course/<int:course_id>/tasks/<int:task_id>', assignments.exam_evaluation, name='exams'),
     path('course/<int:course_id>/tasks/<int:task_id>/list',assignments.task, name='task'),
+    path('course/<int:course_id>/quizzes', quizzes.home, name='quizzes'),
+    path('course/<int:course_id>/quizzes/create/', quizzes.create, name= 'quizzes-create'),
+    path('course/<int:course_id>/quizzes/vote/<poll_id>/', quizzes.vote, name= 'quizzes-vote'),
+    path('course/<int:course_id>/quizzes/results/<poll_id>/', quizzes.results, name= 'quizzes-results'),
 ]
